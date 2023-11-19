9f8ff3be8607d2a79cf022ab621b1035273d7f28
Greg <gregjsmith@gmx.com>
2023-11-19 22:29:11 +0200

    wip log rotate stuff

--- geoip-enrichment/config/logrotater/logrotater.sh
+++ geoip-enrichment/config/logrotater/logrotater.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-mv /var/log/syslog/syslog.log /var/log/syslog/syslog.log.1
-touch /var/log/syslog/syslog.log && chown abc:users /var/log/syslog/syslog.log
+cp /var/log/syslog/syslog.log /var/log/syslog/syslog.old
+touch /var/log/syslog/syslog.log
+chown abc:users /var/log/syslog/syslog.log
+#touch /root/test.txt
+
+#/bin/date >> /root/test.txt
 
