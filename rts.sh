e90f1b6a153821e1c198d5966a1ef6757657ff28
Greg <gregjsmith@gmx.com>
2023-12-07 04:25:37 +0200

    method to rotate log done, I HOPE, see rts.sh for instructions

---
+++ rts.sh
@@ -0,0 +1,7 @@
+1. Need to run sed command
+2. Need to move syslog.log syslog.log.old
+3. Need to touch syslog.log
+4. Need to remove syslog-ng container
+5. Need to remvoe promtail container
+
+!!! Check note on desk for confirmation of steps
